812c4871eaa3bb645b6fb3c6c2cbb4b701aa8297
Francisco Fermin Perez Mendoza <franco.mza123@gmail.com>
2022-06-02 15:01:20 -0500

    Aletas, agregar item, validacion form y total orden

--- src/main.js
+++ src/main.js
@@ -5,4 +5,16 @@ import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
-createApp(App).mount('#app')
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
+
+const options = {
+    confirmButtonColor: '#41b882',
+    cancelButtonColor: '#ff7674',
+};
+
+const app = createApp(App)
+
+app.use(VueSweetalert2, options);
+
+app.mount('#app');
--- src/pages/Listado.vue
+++ src/pages/Listado.vue
@@ -1,94 +1,217 @@
 <template>
-  <br>
-  <h3>Revision de Orden</h3>
-  <h5>Numero: {{ order.number }}</h5>
-  <div style="float: right;">
-    <button type="button" class="btn btn-success" @click="showModal = true">Agregar</button>
+
+<!-- DIV LISTA -->
+  <div v-if="list">
+    <br>
+    <h3>Revision de Orden</h3>
+    <h5>Numero: {{ order.number }}</h5>
+    <!-- DIV LOADING -->
+      <div v-if="loading">
+            <img src="../assets/preloader.gif" alt="loading" style="width: 50px;display:block;margin:auto;">
+      </div>
+    <!--  -->
+
+    <!-- BOTON AGREGAR -->
+      <div style="float: right;">
+        <button type="button" class="btn btn-success" @click="form = true;list = false">Agregar Item</button>
+      </div>
+    <!--  -->
+    <br>
+    <!-- DVI DE LA TABLA -->
+      <div class="border" style="margin-top: 15px;height: 300px;">
+        <table class="table table-sm table-striped">
+          <thead>
+              <tr>
+                  <th style="width: 20%;">SKU</th>
+                  <th style="width: 50%;">Nombre</th>
+                  <th style="width: 15%;">Cantidad</th>
+                  <th style="width: 15%;">Precio</th>
+              </tr>
+          </thead>
+          <tbody>
+              <tr v-for="itemOrder in order.items">
+                  <td>{{ itemOrder.sku }}</td>
+                  <td>{{ itemOrder.name }}</td>
+                  <td>{{ itemOrder.quantity }}</td>            
+                  <td>{{ itemOrder.price }}</td>
+              </tr>
+          </tbody>
+        </table>
+      </div>
+    <!--  -->
+
+    <!-- DIV TOTAL ORDEN -->
+      <div class="row" style="padding: 5px 0px 0px 0px;margin: 0px;background-color: #f3f3f3;">
+        <div style="width: 85%;"><b style="float: right;">Total de la orden:</b></div>
+        <div style="width: 15%;"> {{ total_order }} </div>
+      </div>
+    <!--  -->
+    <br>
+    <!-- bOTON PAGO -->
+      <div style="float: right;">
+        <button class="btn btn-success" @click="payOrder();">Pagar Orden</button>
+      </div>
+    <!--  -->
+    
+  </div> 
+<!--  -->
+
+<!-- DIV FORM -->
+  <div v-if="form">
+      <br>
+      <h3>Agregar Nuevo Articulo</h3>
+      <br>
+      <!-- BOTON VOLVER -->
+        <div style="float: right;">
+          <button class="btn btn-primary" @click="hideForm()">Volver</button>
+        </div>
+      <!--  -->
+      <br>
+      <!-- INPUTS -->
+        <div class="row">
+          <div class="col-3">
+            <label for="sku"><b>SKU</b></label>
+            <input id="sku" name="sku" type="text" class="form-control" v-bind:class="{ errorInput: alert_sku }" v-model="sku" required>
+            <small v-if="alert_sku" class="text-danger">*SKU bligatorio</small>
+          </div>
+          <div class="col-3">
+            <label for="name"><b>Nombre</b></label>
+            <input id="name" name="name" type="text" class="form-control" v-bind:class="{ errorInput: alert_name }" v-model="name" required>
+            <small v-if="alert_name" class="text-danger">*Nombre bligatorio</small>
+          </div>
+          <div class="col-3">
+              <label for="quantity"><b>Cantidad</b></label>
+                <input id="quantity" name="quantity" type="text" class="form-control" v-bind:class="{ errorInput: alert_quantity }" v-model="quantity" required>
+                <small v-if="alert_quantity" class="text-danger">*Cantidad bligatorio</small>
+          </div>
+          <div class="col-3">
+            <label for="price"><b>Precio</b></label>
+            <input id="price" name="price" type="text" class="form-control" v-bind:class="{ errorInput: alert_price }" v-model="price" required>
+            <small v-if="alert_price" class="text-danger">*Precio bligatorio</small>
+          </div>
+        </div>
+      <!--  -->
+      <br>
+      <!-- DIV BOTON GUARDAR -->
+        <div style="float: right;">
+          <button class="btn btn-success" @click="addItem()">Guardar</button>
+        </div>
+      <!--  -->
   </div>
-  <table class="table table-sm table-striped">
-    <thead>
-        <tr>
-            <th>SKU</th>
-            <th>NAME</th>
-            <th>QUANTITY</th>
-            <th>PRICE</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr v-for="itemOrder in order.items">
-            <td>{{ itemOrder.sku }}</td>
-            <td>{{ itemOrder.name }}</td>
-            <td>{{ itemOrder.quantity }}</td>            
-            <td>{{ itemOrder.price }}</td>
-        </tr>
-    </tbody>
-  </table>
-
-  <Teleport to="body">
-    <!-- use the modal component, pass in the prop -->
-    <modal :show="showModal" @close="showModal = false">
-      <template #header>
-        <h3>custom header</h3>
-      </template>
-    </modal>
-  </Teleport>
-
+<!--  -->
 </template>
 
 <script>
-  import axios from "axios"; 
-  import Modal from '../components/Modal.vue'
-  
+  import axios from "axios";
+  import Swal from 'sweetalert2'
         
   export default {
-    components: {
-      Modal
-    },
-    props: {
-      sku:{
-        type: String, 
-        required: true
-      },
-      name: {
-        type: String, 
-        required: true
-      },
-      quantity: {
-        type: Number, 
-        required: true
-      },
-      price: {
-        type: Number, 
-        required: true
-      },
-    },
     data() {
       return {
         url : "https://eshop-deve.herokuapp.com/api/v2/orders",
         header: "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJwUGFINU55VXRxTUkzMDZtajd ZVHdHV3JIZE81cWxmaCIsImlhdCI6MTYyMDY2Mjk4NjIwM30.lhfzSXW9_TC67SdDKyD bMOYiYsKuSk6bG6XDE1wz2OL4Tq0Og9NbLMhb0LUtmrgzfWiTrqAFfnPldd8QzWvgVQ",
         order: [],
-        showModal: false
+        form: false,
+        list: true,
+        loading: false,
+        sku: "",
+        alert_sku: false,
+        name: "",
+        alert_name: false,
+        quantity: "",
+        alert_quantity: false,
+        price: "",
+        alert_price: false,
+        total_order: 0
       }
     },
     methods: {
-      listarProductos(){
+      prodcutList(){
+        this.loading = true;
         axios.get(this.url, { 'headers': { 'Authorization': this.header } })
         .then((response) => {
             this.order = response.data.orders[0];
-            console.log(response.data.orders[0])
+            this.loading = false;
+            this.price_accumulated();
         })
         .catch((error) => {
             console.log(error)
+            this.loading = false;
+        })        
+      },
+
+      hideForm(){
+        this.sku = "";
+        this.alert_sku = "";
+        this.name = "";
+        this.quantity = "";
+        this.price = "";
+
+        this.form = false;
+        this.list = true;
+
+        this.price_accumulated();
+      },
+
+      addItem() {
+        const sku = this.sku;
+        const name = this.name;
+        const quantity = this.quantity;
+        const price = this.price;
+
+        if(sku=="" || name=="" || quantity=="" || price==""){
+          if (sku=="") { this.alert_sku = true } else { this.alert_sku = false }
+          if (name=="") { this.alert_name = true } else { this.alert_name = false }
+          if (quantity=="") { this.alert_quantity = true } else { this.alert_quantity = false }
+          if (price=="") { this.alert_price = true } else { this.alert_price = false }
+          return
+        }
+
+        const new_item = {
+          sku: sku,
+          name: name,
+          quantity: quantity,
+          price: parseFloat(price).toFixed(2)
+        };
+
+        this.order.items.push(new_item);
+        
+        this.hideForm();
+      },
+
+      payOrder() {
+        Swal.fire({
+          title: '¿Seguro que quieres pagar esta Orden por: $'+this.total_order+'?',
+          showDenyButton: true,
+          confirmButtonText: 'Sí',
+          confirmButtonColor: 'green',
+          denyButtonText: `No`,
+          icon: `warning`
+        }).then((result) => {
+          if (result.isConfirmed) {
+            Swal.fire('Exito', 'Se pago la orden correctamente.', 'success')
+          } else if (result.isDenied) {
+            
+          }
         })
       },
-      agregarItem() {
-          console.log("modal");
-      },
+
+      price_accumulated() {
+        let accumulated = 0;
+        this.order.items.map((item) =>{
+          let price = item.price;
+          price = parseFloat(price).toFixed(2);
+          price = parseFloat(price);
+          accumulated = parseFloat(accumulated) + price;
+          accumulated =  Intl.NumberFormat('es-MX',{currency:'MXN',minimumFractionDigits: 2,maximumFractionDigits: 2}).format(accumulated);
+        })
+        this.total_order = accumulated;
+      }
       
     },
     created(){
-      this.listarProductos()
-    }
+      this.prodcutList()
+    },
 
 
   };
@@ -97,11 +220,15 @@
 
 <style scoped>
   button{
-    width: 100px;
+    width: 120px;
     height: 30px;
     padding: 0px;
   }
   .colOptions{
     width: 120px;
+  }
+  .errorInput{
+    border-color: red;
+    background-color: white;
   }
 </style>
